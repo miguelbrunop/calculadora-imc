a632e9ccc734257d8f846b8f257cfed905f3b5da
miguelbrunop <99031323+miguelbrunop@users.noreply.github.com>
2022-04-20 15:36:51 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,37 @@
+const calcular = document.getElementById('calcular');
+
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }else if (valorIMC < 25) {
+            classificacao = 'com o peso ideal.';
+        }else if (valorIMC < 30) {
+            classificacao = 'com sobrepeso.';
+        }else if (valorIMC < 35) {
+            classificacao = 'com Obesidade Grau I.';
+        }else if (valorIMC < 40){
+            classificacao = 'com Obesidade Grau II.';
+        }else {
+            classificacao = 'com Obesidade Grau III. Cuidado.';
+        }
+
+        resultado.textContent = `${nome}, seu IMC é: ${valorIMC} Você está ${classificacao}`;
+
+    } else {
+        alert('Preencha todos os campos!');
+    }
+
+}
+
+calcular.addEventListener('click', imc);
